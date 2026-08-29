b3836dc72c8645eeffb1fda080ddb44f3c385740
gyanbouddha66-art <gyanbouddha66@gmail.com>
2026-08-29 17:55:01 +0530

    Update gemini_ai.py

--- gemini_ai.py
+++ gemini_ai.py
@@ -1,1 +1,23 @@
 
+import os
+from google import genai
+
+
+def ask_gemini(prompt):
+    api_key = os.getenv("GEMINI_API_KEY")
+
+    if not api_key:
+        return "Gemini API key configured nahi hai."
+
+    try:
+        client = genai.Client(api_key=api_key)
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt
+        )
+
+        return response.text
+
+    except Exception as e:
+        return f"Gemini Error: {e}"
