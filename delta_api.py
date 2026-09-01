b434efc53c0fb052bc8fd38afe49e0ccb5a50e81
gyanbouddha66-art <gyanbouddha66@gmail.com>
2026-09-01 12:48:18 +0530

    Update delta_api.py

--- delta_api.py
+++ delta_api.py
@@ -20,29 +20,25 @@ Last hour
 
 
 
-📥 Incoming Telegram Data: {'update_id': 74057849, 'message': {'message_id': 2777, 'from': {'id': 965643127, 'is_bot': False, 'first_name': 'Santosh', 'last_name': 'Mali', 'language_code': 'en'}, 'chat': {'id': 965643127, 'first_name': 'Santosh', 'last_name': 'Mali', 'type': 'private'}, 'date': 1788236462, 'text': '/start', 'entities': [{'offset': 0, 'length': 6, 'type': 'bot_command'}]}}
-127.0.0.1 - - [01/Sep/2026:06:38:36 +0000] "POST / HTTP/1.1" 200 2 "-" "-"
-📥 Incoming Telegram Data: {'update_id': 74057850, 'callback_query': {'id': '4147405651649222262', 'from': {'id': 965643127, 'is_bot': False, 'first_name': 'Santosh', 'last_name': 'Mali', 'language_code': 'en'}, 'message': {'message_id': 2783, 'from': {'id': 8919168139, 'is_bot': True, 'first_name': 'gyanchand_delta_bot', 'username': 'gyanchand_delta_bot'}, 'chat': {'id': 965643127, 'first_name': 'Santosh', 'last_name': 'Mali', 'type': 'private'}, 'date': 1788244716, 'text': '🧠 GH BOSS AI — SMART TRADING SYSTEM\n\n⚙️ Current Mode: MANUAL\n✅ Telegram, Groq AI & Delta API Connected\n🎤 Voice & Buttons Enabled', 'reply_markup': {'inline_keyboard': [[{'text': '🟢 BUY (LONG)', 'callback_data': 'btn_buy'}, {'text': '🔴 SELL (SHORT)', 'callback_data': 'btn_sell'}], [{'text': '⚙️ Mode: MANUAL', 'callback_data': 'btn_toggle_mode'}, {'text': '📊 Refresh Signal', 'callback_data': 'btn_signal'}]]}}, 'chat_instance': '4018111906769970331', 'data': 'btn_buy'}}
-127.0.0.1 - - [01/Sep/2026:06:40:10 +0000] "POST / HTTP/1.1" 200 2 "-" "-"
-📥 Incoming Telegram Data: {'update_id': 74057851, 'callback_query': {'id': '4147405653311211672', 'from': {'id': 965643127, 'is_bot': False, 'first_name': 'Santosh', 'last_name': 'Mali', 'language_code': 'en'}, 'message': {'message_id': 2783, 'from': {'id': 8919168139, 'is_bot': True, 'first_name': 'gyanchand_delta_bot', 'username': 'gyanchand_delta_bot'}, 'chat': {'id': 965643127, 'first_name': 'Santosh', 'last_name': 'Mali', 'type': 'private'}, 'date': 1788244716, 'text': '🧠 GH BOSS AI — SMART TRADING SYSTEM\n\n⚙️ Current Mode: MANUAL\n✅ Telegram, Groq AI & Delta API Connected\n🎤 Voice & Buttons Enabled', 'reply_markup': {'inline_keyboard': [[{'text': '🟢 BUY (LONG)', 'callback_data': 'btn_buy'}, {'text': '🔴 SELL (SHORT)', 'callback_data': 'btn_sell'}], [{'text': '⚙️ Mode: MANUAL', 'callback_data': 'btn_toggle_mode'}, {'text': '📊 Refresh Signal', 'callback_data': 'btn_signal'}]]}}, 'chat_instance': '4018111906769970331', 'data': 'btn_signal'}}
-127.0.0.1 - - [01/Sep/2026:06:41:02 +0000] "POST / HTTP/1.1" 200 2 "-" "-"
-📥 Incoming Telegram Data: {'update_id': 74057852, 'callback_query': {'id': '4147405653028495569', 'from': {'id': 965643127, 'is_bot': False, 'first_name': 'Santosh', 'last_name': 'Mali', 'language_code': 'en'}, 'message': {'message_id': 2783, 'from': {'id': 8919168139, 'is_bot': True, 'first_name': 'gyanchand_delta_bot', 'username': 'gyanchand_delta_bot'}, 'chat': {'id': 965643127, 'first_name': 'Santosh', 'last_name': 'Mali', 'type': 'private'}, 'date': 1788244716, 'text': '🧠 GH BOSS AI — SMART TRADING SYSTEM\n\n⚙️ Current Mode: MANUAL\n✅ Telegram, Groq AI & Delta API Connected\n🎤 Voice & Buttons Enabled', 'reply_markup': {'inline_keyboard': [[{'text': '🟢 BUY (LONG)', 'callback_data': 'btn_buy'}, {'text': '🔴 SELL (SHORT)', 'callback_data': 'btn_sell'}], [{'text': '⚙️ Mode: MANUAL', 'callback_data': 'btn_toggle_mode'}, {'text': '📊 Refresh Signal', 'callback_data': 'btn_signal'}]]}}, 'chat_instance': '4018111906769970331', 'data': 'btn_sell'}}
-127.0.0.1 - - [01/Sep/2026:06:41:13 +0000] "POST / HTTP/1.1" 200 2 "-" "-"
-127.0.0.1 - - [01/Sep/2026:06:41:23 +0000] "HEAD / HTTP/1.1" 200 0 "https://delta-telegram-bot-agg7.onrender.com" "Mozilla/5.0+(compatible; UptimeRobot/2.0; http://www.uptimerobot.com/)"
-📥 Incoming Telegram Data: {'update_id': 74057853, 'message': {'message_id': 2787, 'from': {'id': 965643127, 'is_bot': False, 'first_name': 'Santosh', 'last_name': 'Mali', 'language_code': 'en'}, 'chat': {'id': 965643127, 'first_name': 'Santosh', 'last_name': 'Mali', 'type': 'private'}, 'date': 1788244889, 'text': 'क्या कर रहे हो'}}
-GROQ MODELS STATUS: 200
-AVAILABLE GROQ MODELS: ['groq/compound-mini', 'meta-llama/llama-prompt-guard-2-22m', 'openai/gpt-oss-20b', 'openai/gpt-oss-120b', 'qwen/qwen3.8-27b', 'allam-2-7b', 'canopylabs/orpheus-arabic-saudi', 'whisper-large-v3-turbo', 'canopylabs/orpheus-v1-english', 'groq/compound', 'whisper-large-v3', 'meta-llama/llama-prompt-guard-2-86m', 'qwen/qwen3.6-27b', 'openai/gpt-oss-safeguard-20b']
-✅ SELECTED GROQ MODEL: openai/gpt-oss-120b
-GROQ STATUS: 200
-127.0.0.1 - - [01/Sep/2026:06:41:57 +0000] "POST / HTTP/1.1" 200 2 "-" "-"
-==> Detected service running on port 10000
-==> Docs on specifying a port: https://render.com/docs/web-services#port-binding
-127.0.0.1 - - [01/Sep/2026:06:46:27 +0000] "HEAD / HTTP/1.1" 200 0 "https://delta-telegram-bot-agg7.onrender.com" "Mozilla/5.0+(compatible; UptimeRobot/2.0; http://www.uptimerobot.com/)"
-127.0.0.1 - - [01/Sep/2026:06:51:31 +0000] "HEAD / HTTP/1.1" 200 0 "https://delta-telegram-bot-agg7.onrender.com" "Mozilla/5.0+(compatible; UptimeRobot/2.0; http://www.uptimerobot.com/)"
-127.0.0.1 - - [01/Sep/2026:06:56:37 +0000] "HEAD / HTTP/1.1" 200 0 "https://delta-telegram-bot-agg7.onrender.com" "Mozilla/5.0+(compatible; UptimeRobot/2.0; http://www.uptimerobot.com/)"
-127.0.0.1 - - [01/Sep/2026:07:01:40 +0000] "HEAD / HTTP/1.1" 200 0 "https://delta-telegram-bot-agg7.onrender.com" "Mozilla/5.0+(compatible; UptimeRobot/2.0; http://www.uptimerobot.com/)"
+==> Exited with status 1
+==> Common ways to troubleshoot your deploy: https://render.com/docs/troubleshooting-deploys
+==> Running 'gunicorn app:app --workers 1 --threads 2 --timeout 120'
+127.0.0.1 - - [01/Sep/2026:07:06:46 +0000] "HEAD / HTTP/1.1" 200 0 "https://delta-telegram-bot-agg7.onrender.com" "Mozilla/5.0+(compatible; UptimeRobot/2.0; http://www.uptimerobot.com/)"
 ==> Deploying...
 ==> Setting WEB_CONCURRENCY=1 by default, based on available CPUs in the instance
+==> Running 'gunicorn app:app --workers 1 --threads 2 --timeout 120'
+Traceback (most recent call last):
+  File "/opt/render/project/src/.venv/bin/gunicorn", line 8, in <module>
+    sys.exit(run())
+  File "/opt/render/project/src/.venv/lib/python3.10/site-packages/gunicorn/app/wsgiapp.py", line 66, in run
+    WSGIApplication("%(prog)s [OPTIONS] [APP_MODULE]", prog=prog).run()
+  File "/opt/render/project/src/.venv/lib/python3.10/site-packages/gunicorn/app/base.py", line 235, in run
+    super().run()
+  File "/opt/render/project/src/.venv/lib/python3.10/site-packages/gunicorn/app/base.py", line 71, in run
+    Arbiter(self).run()
+  File "/opt/render/project/src/.venv/lib/python3.10/site-packages/gunicorn/arbiter.py", line 63, in __init__
+    self.setup(app)
+  File "/opt/render/project/src/.venv/lib/python3.10/site-packages/gunicorn/arbiter.py", line 164, in setup
     self.app.wsgi()
   File "/opt/render/project/src/.venv/lib/python3.10/site-packages/gunicorn/app/base.py", line 66, in wsgi
     self.callable = self.load()
@@ -66,10 +62,14 @@ GROQ STATUS: 200
     from trading_engine import get_engine_status, get_signal
   File "/opt/render/project/src/trading_engine.py", line 4, in <module>
     from delta_api import get_live_price, get_candles
-ImportError: cannot import name 'get_live_price' from 'delta_api' (/opt/render/project/src/delta_api.py)
+  File "/opt/render/project/src/delta_api.py", line 23
+    📥 Incoming Telegram Data: {'update_id': 74057849, 'message': {'message_id': 2777, 'from': {'id': 965643127, 'is_bot': False, 'first_name': 'Santosh', 'last_name': 'Mali', 'language_code': 'en'}, 'chat': {'id': 965643127, 'first_name': 'Santosh', 'last_name': 'Mali', 'type': 'private'}, 'date': 1788236462, 'text': '/start', 'entities': [{'offset': 0, 'length': 6, 'type': 'bot_command'}]}}
+    ^
+SyntaxError: invalid character '📥' (U+1F4E5)
 ==> Exited with status 1
 ==> Common ways to troubleshoot your deploy: https://render.com/docs/troubleshooting-deploys
 ==> Running 'gunicorn app:app --workers 1 --threads 2 --timeout 120'
+127.0.0.1 - - [01/Sep/2026:07:11:51 +0000] "HEAD / HTTP/1.1" 200 0 "https://delta-telegram-bot-agg7.onrender.com" "Mozilla/5.0+(compatible; UptimeRobot/2.0; http://www.uptimerobot.com/)"
 0 services selected:
 
 Move
